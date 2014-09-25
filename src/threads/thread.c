5307ad9b0322a875ce5caa13128ec73d7da7038c
calvinp <calvinpom@gmail.com>
2014-09-24 21:26:53 -0400

    Finished except for proofreading

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -69,7 +69,7 @@ static void kernel_thread (thread_func *, void *aux);
 static void idle (void *aux UNUSED);
 static struct thread *running_thread (void);
 static struct thread *next_thread_to_run (void);
-static void init_thread (struct thread *t, const char *name, int nice, int  priority);
+static void init_thread (struct thread *t, const char *name, int  priority);
 static bool is_thread (struct thread *) UNUSED;
 static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
@@ -101,7 +101,7 @@ thread_init (void)
 
   /* Set up a thread structure for the running thread. */
   initial_thread = running_thread ();
-  init_thread (initial_thread, "main", 0, PRI_DEFAULT);
+  init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
 }
@@ -186,7 +186,7 @@ thread_create (const char *name, int priority,
     return TID_ERROR;
 
   /* Initialize thread. */
-  init_thread (t, name, thread_get_nice(), priority);
+  init_thread (t, name, priority);
   tid = t->tid = allocate_tid ();
 
   /* Stack frame for kernel_thread(). */
@@ -448,7 +448,7 @@ thread_get_priority (void)
 
 /* Sets the current thread's nice value to NICE. */
 void
-thread_set_nice (int nice) 
+thread_set_nice (int nice UNUSED) 
 {
   return;
 }
@@ -547,7 +547,7 @@ is_thread (struct thread *t)
 /* Does basic initialization of T as a blocked thread named
    NAME. */
 static void
-init_thread (struct thread *t, const char *name, int nice, int priority)
+init_thread (struct thread *t, const char *name, int priority)
 {
   enum intr_level old_level;
 
